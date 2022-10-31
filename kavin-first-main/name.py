01702f5b5ae565928e19828543b44aa396c8d519
Aniketh Rai <aniketh5798@gmail.com>
2022-11-01 01:17:40 +0530

    statistics code added

--- kavin-first-main/name.py
+++ kavin-first-main/name.py
@@ -1,5 +1,36 @@
-print("hi am kavin")
-print('Hi I am Aniketh')
-print('we are studying Data Science')
-print('Lets begin the project')
+x = [10, 20, 30, 40, 50, 60]
+
+
+def mean(x):
+    mean = (sum(x) / len(x))
+    return mean
+
+
+def median(x):
+    x.sort()
+    q = len(x) // 2
+    if len(x) % 2 != 0:
+        return x[q]
+    elif len(x) % 2 == 0:
+        return (x[q - 1] + x[q]) / 2
+        return median
+
+
+def mode(x):
+    counter = 0
+    num = x[0]
+    for i in x:
+        curr_frequency = x.count(i)
+        if (curr_frequency > counter):
+            counter = curr_frequency
+            num = i
+        if len(set(x)) == len(x):
+            return 'there is no mode'
+    return num
+
+
+def dev(x):
+    for i in x:
+        x.append(i - mean(x))
+    return dev
 
--- main.py
+++ main.py
@@ -1,16 +1,46 @@
+def get_mean(x):
+    return (sum(x) / len(x))
+def get_median(x):
+    x.sort()
+    q=len(x)//2
+    if len(x)%2!=0:
+        return x[q]
+    elif len(x)%2==0:
+        return (x[q-1]+x[q])/2
+    return median
+def get_dev(x,get_mean):
+    return [i-get_mean(x) for i in x]
+def my_abs(i):
+    if i<0:
+        return -i
+    else:
+        return i
+def get_absolute(x):
+    return [my_abs(i) for i in x]
+def get_square(x):
+        return [i**2 for i in x]
+def get_var(x):
+    return sum(x)/len(x)
+def SD(x):
+    return (x)**0.5
+x=[10,20,30,40,50]
+print(get_dev(x,get_mean))
+x=get_dev(x,get_mean)
+print(get_absolute(x))
+x=get_absolute(x)
+print(get_square(x))
+x=get_square(x)
+print(get_var(x))
+x=get_var(x)
+print(SD(x))
 
-print('Enter the integer')
-print('Enter 1 for True and 0 for False:')
-n=int(input())
-b=bool(int(input()))
-boolean=bool(b)
 
-if boolean == True:
-     for i in range(n):
-        print("*"*(i+1))
-else:
-     for i in range(n):
-            print("*"*(n-i))
+
+
+
+
+
+
 
 
 

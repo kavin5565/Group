57759962ea55d570096bb9805004e832a7a963bc
kavin <kavindia005@gmail.com>
2022-11-02 07:59:02 +0530

    course code added 1

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def main():
     print("\t2)To Add New Student")
     print("\t3)To Remove Student")
     print("\t4)To Update Student Details")
-    print("\t5)To Get All Student Details")
+    print("\t5)To Update Academy Name")
+    print("\t6)To Get Number of Students")
+    print("\t7)To Get All Student Details")
+    print("\t8)To Get any Course Student Details")
+
 
     option=input("Enter AAny Above Given Option :")
     print()
@@ -25,8 +29,29 @@ class student:
         self.Register_No=input("\tEnter the Register number :")
         self.Mobile_No=input("\tEnter the Mobile Number :")
         self.Address=input("\tEnter the Address :")
-        self.Course=input("\tEnter the Course :")
+        course=input("\tEnter the Course :"[Ex: JAVA,Python,C,C++,RUST,Data_Science,Devops])
         self.Fees=input("\tEnter the actual Fees :")
+
+
+        if course in course.field:
+            course.field[course].courselist.append(self)
+        else:
+            new_course=field(course)
+            new_course.courselist.append(self)
+            course.field[course]=new_course
+
+        self.course=course.field[course]
+
+
+
+
+        class course:
+            field={}
+            def __init__(self,Name):
+                self.Name=Name
+                course.field[Name]=self
+                self.courselist=[]
+
 
 
 
